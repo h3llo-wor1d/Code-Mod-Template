d366ba1e1d033cfdcecdf0dc018a7d604da2b47c
Jasmine Stephens <jasminegamedev@gmail.com>
2024-04-05 00:06:04 -0700

    Initial Commit
    Note: this is not done and still needs more work. Just submitting what I have so far.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Code-Mod-Template
-Code Mod Template for the Celeste 64 Fuji Mod Loader
+# Celeste 64 Fuji Code Mod Template
+This is a Template for a code mod for the Celeste 64 Fuji Mod Loader. This template gives you a basic C# project that can be compiled into a code mod.
+
+TODO: Give instructions for setting up Celeste64.dll and compiling this mod.
+
+When you are done making your mod, and are ready to publish it, you can either delete this readme file, or edit it if desired. You can also delete the .gitignore file, and Source folder if desired.
+
+TODO: Add some sort of publish script/tool?
+
+Note: This template is still a work in progress, an may change and evolve over time.
---
+++ Source/TemplateGameMod.cs
@@ -0,0 +1,32 @@
+﻿namespace FujiCodeMod;
+
+using Celeste64;
+using Celeste64.Mod;
+using Foster.Framework;
+
+// For code mods, you must have a class extending from Fuji's GameMod class. This is the entry point of your mod.
+// Rename this class and file to better reflect your own mod.
+public class TemplateGameMod : GameMod
+{
+	// This template includes some common functions:
+	public override void OnModLoaded()
+	{
+		// Ran when the mod is loaded.
+		Log.Info($"Hello from {GetType()}");
+	}
+
+	public override void OnModUnloaded()
+	{
+		// Ran when the mod is unloaded.
+	}
+
+	public override void OnGameLoaded(Game game)
+	{
+		// Ran when the game has finished loading.
+	}
+
+	public override void Update(float deltaTime)
+	{
+		// Ran every frame.
+	}
+}
